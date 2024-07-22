1886d9f5b9d0b39983d22aa7623783913e92a261
BHarish07 <harishbalike1995@gmail.com>
2024-07-22 08:43:42 +0530

    shared-library

---
+++ vars/pipelineGlobals.groovy
@@ -0,0 +1,11 @@
+static final String region(){
+    return "us-east-1"
+}
+
+static final String nexusURL(){
+    return "nexus.harishbalike.online:8081"
+}
+
+static final String accountID(){
+    return "705613225906"
+}
